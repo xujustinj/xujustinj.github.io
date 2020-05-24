58d4094120cd2d2782c2cac4fc6c7ae31f3d035a
Justin Xu <xu.justin.j@gmail.com>
2020-05-24 16:09:11 -0400

    Huge content update

--- src/home.js
+++ src/home.js
@@ -15,7 +15,13 @@ import {
   Visibility,
 } from 'semantic-ui-react'
 
-import JXdp from './img/avatar.png'
+import jx_avatar from './img/jx_avatar.png'
+import adetos from './img/adetos.png'
+import asteria from './img/asteria.png'
+import hexagonal_2048 from './img/hexagonal_2048.png'
+import stairway_constants from './img/stairway_constants.png'
+import swift_record_generator from './img/swift_record_generator.png'
+import ulam_spiral from './img/ulam_spiral.png'
 import './home.css'
 
 // COMMENT FROM semantic-ui-react LAYOUT EXAMPLE:
@@ -29,7 +35,7 @@ const getWidth = () => {
 const Heading = () => (
   <Container text>
     <Header id='homeHeader' inverted>
-      <Image id='homeHeaderAvatar' avatar src={JXdp}/>
+      <Image id='homeHeaderAvatar' avatar src={jx_avatar}/>
       Justin Xu
     </Header>
   </Container>
@@ -181,82 +187,125 @@ const Home = () => (
 
       <Divider hidden/>
       <Divider section horizontal>Current Projects</Divider>
-      <Card.Group centered>
-        <Card raised>
-          <Card.Content as='a' href='https://github.com/xujustinj/Asteria'>
-            <Card.Header>Asteria</Card.Header>
-            <Card.Meta className='labelLine'>
-              <Label>Neural Networks</Label>
+      <Container text>
+        <Card.Group centered>
+          <Card raised>
+            <Image src={asteria} />
+            <Card.Content>
+              <Card.Header as='a' href='https://github.com/xujustinj/Asteria'>Asteria</Card.Header>
+              <Card.Description>
+                <b>Goal:</b> a web app interface for a multilayer perceptron
+                that performs character recognition (implemented from scratch)
+                <br/>
+                <b>So far:</b> an in-browser interactive training/testing engine
+                <br/>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <Label>AI</Label>
               <Label>React</Label>
               <Label>TypeScript</Label>
-              <Label>Sass</Label>
               <Label>C++</Label>
-            </Card.Meta>
-            <Card.Description>
-              <b>Goal:</b> a multilayer perceptron that learns character
-              recognition, implemented from scratch <br/>
-              <b>So far:</b> a web app that runs an interactive training/testing
-              engine in the client <br/>
-              <b>Soon:</b> migrating the engine to a C++ server
-            </Card.Description>
-          </Card.Content>
-        </Card>
-        <Card raised>
-          <Card.Content>
-            <Card.Header>Stairway Constants</Card.Header>
-            <Card.Meta className='labelLine'>
+            </Card.Content>
+          </Card>
+          <Card raised>
+            <Image src={stairway_constants} />
+            <Card.Content>
+              <Card.Header>Stairway Constants</Card.Header>
+              <Card.Description>
+                A massive guide to every constant featured on the decorative
+                number line recently installed on UW campus, published across 5
+                installments of UW mathNEWS volume 142.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
               <Label>Writing</Label>
-              <Label>Recreational Mathematics</Label>
-            </Card.Meta>
-            <Card.Description>
-              A massive guide to every constant featured on the decorative
-              number line recently installed at UW campus.
-              <br /><br />
-              Published across 5 installments of UW mathNEWS volume 142.
-            </Card.Description>
-          </Card.Content>
-        </Card>
-      </Card.Group>
+              <Label>Mathematics</Label>
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </Container>
 
       <Divider hidden/>
       <Divider section horizontal>Past Projects (Section Under Construction)</Divider>
+      <Container text>
       <Card.Group centered>
         <Card raised>
-          <Card.Content as='a' href='https://github.com/xujustinj/Swift-Record-Generator'>
-            <Card.Header>Swift Record Generator</Card.Header>
-            <Card.Meta className='labelLine'>
-              <Label>Swift</Label>
-            </Card.Meta>
+          <Image src={swift_record_generator} />
+          <Card.Content>
+            <Card.Header as='a' href='https://github.com/xujustinj/Swift-Record-Generator'>
+              Swift Record Generator
+            </Card.Header>
             <Card.Description>
               Measures the relative performance of Swift structs, classes, and
               string-keyed dictionaries, with respect to mutation, copying,
               (de)serialization, and more.
             </Card.Description>
           </Card.Content>
+          <Card.Content extra>
+            <Label>Swift</Label>
+          </Card.Content>
         </Card>
         <Card raised>
-          <Card.Content as='a' href='https://xujustinj.github.io/Extended-Essay/'>
-            <Card.Header>Ulam Spiral</Card.Header>
-            <Card.Meta className='labelLine'>
-              <Label>Recreational Math</Label>
-              <Label>Number Theory</Label>
-              <Label>Java</Label>
-            </Card.Meta>
+          <Image src={ulam_spiral} />
+          <Card.Content>
+            <Card.Header as='a' href='https://xujustinj.github.io/Extended-Essay/'>
+              Ulam Spiral
+            </Card.Header>
             <Card.Description>
               When you highlight prime numbers on a spiral number line, strange
-              patterns appear. In high school, I wrote a paper explaining what
-              causes them, and how they can be exploited.
+              patterns appear. In high school, I wrote a <a href='https://xujustinj.github.io/Extended-Essay/03-01-19%20Version%20(Revision).pdf'>
+                paper
+              </a> explaining what causes them, and how they could be exploited.
             </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Label>Number Theory</Label>
+            <Label>Java</Label>
+          </Card.Content>
+        </Card>
+        <Card raised>
+          <Image src={hexagonal_2048} />
+          <Card.Content>
+            <Card.Header as='a' href='https://xujustinj.github.io/Hexagonal-2048/'>
+              Hexagonal 2048
+            </Card.Header>
+            <Card.Description>
+              My introductory project to JavaScript, inspired by my love for the
+              original game.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Label>JavaScript</Label>
+            <Label>p5.js</Label>
+          </Card.Content>
+        </Card>
+        <Card raised>
+          <Image src={adetos} />
+          <Card.Content>
+            <Card.Header as='a' href='https://docs.google.com/spreadsheets/d/1JsttjsZt6USt7h1Z66PhwibUmafxuH1Mwkzk2tNlDag/edit'>
+              ADETOS
+            </Card.Header>
+            <Card.Description>
+              A macro-powered spreadsheet that juggles matchmaking,
+              scorekeeping, and table management for multiple concurrent double
+              elimination tournaments.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Label>Spreadsheets</Label>
+            <Label>Google Apps Script</Label>
           </Card.Content>
         </Card>
       </Card.Group>
+      </Container>
     </Segment>
 
     <Segment id='homeFooter' inverted vertical>
       <Container text>
         <small>
           Made with React and Semantic UI.<br/>
-          Last updated Sunday May 10, 2020.
+          Last updated Sunday May 24, 2020.
         </small>
       </Container>
     </Segment>
