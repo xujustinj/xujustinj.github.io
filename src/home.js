1207815e617ee35a3bd5820fc586f34932b14afd
Justin Xu <xu.justin.j@gmail.com>
2021-05-18 07:02:59 -0400

    Update content and remove spaghetti

--- src/home.js
+++ src/home.js
@@ -1,5 +1,4 @@
-import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
 import {
   Card,
   Container,
@@ -9,168 +8,176 @@ import {
   Image,
   Label,
   Menu,
-  Responsive,
   Segment,
-  Sidebar,
-  Visibility,
 } from "semantic-ui-react";
-
-import jx_avatar from "./img/jx_formal.png";
-import adetos from "./img/adetos.png";
-import asteria from "./img/asteria.png";
-import hexagonal_2048 from "./img/hexagonal_2048.png";
-import stairway_constants from "./img/stairway_constants.png";
-import swift_record_generator from "./img/swift_record_generator.png";
-import ulam_spiral from "./img/ulam_spiral.png";
 import "./home.css";
+
+import FormalAvatar from "./img/formal-avatar.png";
+import ADETOSIcon from "./img/adetos-icon.png";
+import AsteriaIcon from "./img/asteria-icon.png";
+import Hexagonal2048Icon from "./img/hexagonal-2048-icon.png";
+import StairwayConstantsIcon from "./img/stairway-constants-icon.png";
+import SwiftRecordGeneratorIcon from "./img/swift-record-generator-icon.png";
+import UlamSpiralIcon from "./img/ulam-spiral-icon.png";
+import WaterparkIcon from "./img/waterpark-icon.png";
+
+const NavBar = () => (
+  <Menu borderless inverted>
+    <Menu.Item as="a" href="https://github.com/xujustinj/" icon="github">
+      <Icon name="github" />
+      &nbsp; GitHub
+    </Menu.Item>
+    <Menu.Item
+      as="a"
+      href="https://drive.google.com/file/d/1Gmz2ra2j20rVt_x6TgdV845ySR1QhyoW/view"
+    >
+      <Icon name="newspaper" /> Résumé
+    </Menu.Item>
+    <Menu.Item as="a" href="https://www.linkedin.com/in/xujustinj/">
+      <Icon name="linkedin" /> LinkedIn
+    </Menu.Item>
+  </Menu>
+);
+
+const Head = () => (
+  <Segment id="navbar" inverted textAlign="center" vertical>
+    <NavBar />
+    <Container text>
+      <Header id="title" inverted>
+        <Image id="avatar" avatar src={FormalAvatar} />
+        Justin Xu
+      </Header>
+    </Container>
+  </Segment>
+);
+
+const Foot = () => (
+  <Segment id="foot" inverted vertical>
+    <Container text>
+      <small>
+        Made with React and Semantic UI.
+        <br />
+        Last updated Monday May 17, 2021.
+      </small>
+    </Container>
+  </Segment>
+);
+
+const ProjectCard = (props) => {
+  const { iconURL, projectURL, title, description, labels } = props.project;
+  return (
+    <Card raised>
+      <Image src={iconURL} />
+      <Card.Content>
+        <Card.Header as={projectURL && "a"} href={projectURL}>
+          {title}
+        </Card.Header>
+        <Card.Description>{description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        {labels.map((label) => (
+          <Label key={label.name} color={label.color}>
+            {label.name}
+          </Label>
+        ))}
+      </Card.Content>
+    </Card>
+  );
+};
 
 const languageColor = "pink";
 const toolColor = "orange";
 const techniqueColor = "blue";
 
-const githubLink = "https://github.com/xujustinj";
-const resumeLink =
-  "https://drive.google.com/file/d/1kjVQrYlafipLhmWzyLukmeycqPpBqPz0/view?usp=sharing";
-const linkedinLink = "https://www.linkedin.com/in/xujustinj/";
-const emailLink = "mailto:justin.xu@uwaterloo.ca";
-
-// COMMENT FROM semantic-ui-react LAYOUT EXAMPLE:
-// Heads up!
-// We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
-// For more advanced usage please check Responsive docs under the "Usage" section.
-const getWidth = () => {
-  return typeof window === "undefined"
-    ? Responsive.onlyTablet.minWidth
-    : window.innerWidth;
+const ADETOS = {
+  iconURL: ADETOSIcon,
+  projectURL:
+    "https://docs.google.com/spreadsheets/d/1JsttjsZt6USt7h1Z66PhwibUmafxuH1Mwkzk2tNlDag/edit",
+  title: "ADETOS",
+  description:
+    "A macro-powered spreadsheet that juggles matchmaking, scorekeeping, and table management for multiple concurrent double elimination tournaments.",
+  labels: [
+    { name: "Google Sheets", color: toolColor },
+    { name: "Google Apps Script", color: languageColor },
+  ],
 };
-
-const Heading = () => (
-  <Container text>
-    <Header id="homeHeader" inverted>
-      <Image id="homeHeaderAvatar" avatar src={jx_avatar} />
-      Justin Xu
-    </Header>
-  </Container>
-);
-
-const DesktopHeading = (props) => {
-  const { children } = props;
-
-  const [overlay, setOverlay] = useState(false);
-
-  return (
-    <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
-      <Visibility
-        once={false}
-        onBottomPassed={() => setOverlay(true)}
-        onBottomPassedReverse={() => setOverlay(false)}
-      >
-        <Segment id="homeHeaderDesktop" inverted textAlign="center" vertical>
-          <Menu
-            fixed={overlay ? "top" : null}
-            inverted={!overlay}
-            pointing={!overlay}
-            secondary={!overlay}
-            size="large"
-          >
-            <Menu.Item as="a" href={githubLink} icon="github">
-              <Icon name="github" />
-              &nbsp; GitHub
-            </Menu.Item>
-            <Menu.Item as="a" href={resumeLink}>
-              <Icon name="newspaper" /> Résumé
-            </Menu.Item>
-            <Menu.Item as="a" href={linkedinLink}>
-              <Icon name="linkedin" /> LinkedIn
-            </Menu.Item>
-            <Menu.Item as="a" href={emailLink}>
-              <Icon name="mail" /> Email
-            </Menu.Item>
-          </Menu>
-          <Heading />
-        </Segment>
-      </Visibility>
-
-      {children}
-    </Responsive>
-  );
+const Asteria = {
+  iconURL: AsteriaIcon,
+  projectURL: "https://github.com/xujustinj/Asteria",
+  title: "Asteria",
+  description:
+    "A fast machine learning library implemented from scratch. Currently working towards character recognition.",
+  labels: [
+    { name: "AI", color: techniqueColor },
+    { name: "C++", color: languageColor },
+    { name: "Python", color: languageColor },
+  ],
 };
-DesktopHeading.propTypes = {
-  children: PropTypes.node,
+const Hexagonal2048 = {
+  iconURL: Hexagonal2048Icon,
+  projectURL: "https://xujustinj.github.io/Hexagonal-2048/",
+  title: "Hexagonal 2048",
+  description:
+    "My introductory project to JavaScript, inspired by my love for the original game. Swipe or use QWEASD to control the board.",
+  labels: [
+    { name: "JavaScript", color: languageColor },
+    { name: "p5.js", color: toolColor },
+  ],
 };
-
-const MobileHeading = (props) => {
-  const { children } = props;
-
-  const [sidebar, setSidebar] = useState(false);
-
-  return (
-    <Responsive
-      as={Sidebar.Pushable}
-      getWidth={getWidth}
-      maxWidth={Responsive.onlyMobile.maxWidth}
-    >
-      <Sidebar
-        as={Menu}
-        animation="push"
-        inverted
-        onHide={() => setSidebar(false)}
-        vertical
-        visible={sidebar}
-      >
-        <Menu.Item as="a" href={githubLink} icon="github">
-          <Icon name="github" /> GitHub
-        </Menu.Item>
-        <Menu.Item as="a" href={resumeLink}>
-          <Icon name="newspaper" /> Résumé
-        </Menu.Item>
-        <Menu.Item as="a" href={linkedinLink}>
-          <Icon name="linkedin" /> LinkedIn
-        </Menu.Item>
-        <Menu.Item as="a" href={emailLink}>
-          <Icon name="mail" /> Email
-        </Menu.Item>
-      </Sidebar>
-
-      <Sidebar.Pusher dimmed={sidebar}>
-        <Segment id="homeHeaderMobile" inverted textAlign="center" vertical>
-          <Container>
-            <Menu inverted pointing secondary size="large">
-              <Menu.Item onClick={() => setSidebar(!sidebar)}>
-                <Icon name="sidebar" />
-              </Menu.Item>
-            </Menu>
-          </Container>
-          <Heading />
-        </Segment>
-
-        {children}
-      </Sidebar.Pusher>
-    </Responsive>
-  );
+const StairwayConstants = {
+  iconURL: StairwayConstantsIcon,
+  title: "Stairway Constants",
+  description:
+    "A massive guide to every constant featured on the decorative number line recently installed on UW campus, published across 5 installments of UW mathNEWS volume 142.",
+  labels: [
+    { name: "Writing", color: techniqueColor },
+    { name: "Mathematics", color: techniqueColor },
+  ],
 };
-MobileHeading.propTypes = {
-  children: PropTypes.node,
+const SwiftRecordGenerator = {
+  iconURL: SwiftRecordGeneratorIcon,
+  projectURL: "https://github.com/xujustinj/swift-record-generator",
+  title: "Swift Record Generator",
+  description:
+    "Measures the relative performance of Swift structs, classes, and string-keyed dictionaries, with respect to mutation, copying, (de)serialization, and more.",
+  labels: [{ name: "Swift", color: languageColor }],
 };
-
-const ResponsiveHeading = ({ children }) => (
-  <div>
-    <DesktopHeading>{children}</DesktopHeading>
-    <MobileHeading>{children}</MobileHeading>
-  </div>
-);
-ResponsiveHeading.propTypes = {
-  children: PropTypes.node,
+const UlamSpiral = {
+  iconURL: UlamSpiralIcon,
+  projectURL: "https://xujustinj.github.io/Extended-Essay/",
+  title: "Ulam Spiral",
+  description:
+    "When you highlight prime numbers on a spiral number line, strange patterns appear. In high school, I wrote a paper explaining what causes them, and how they might be useful.",
+  labels: [
+    { name: "Number Theory", color: techniqueColor },
+    { name: "Java", color: languageColor },
+  ],
+};
+const Waterpark = {
+  iconURL: WaterparkIcon,
+  projectURL: "https://github.com/loolabs/waterpark",
+  title: "Waterpark",
+  description:
+    "Loo Labs' first product: a web app that helps UWaterloo students discover clubs and events. We're overengineering this to the moon!",
+  labels: [
+    { name: "TypeScript", color: languageColor },
+    { name: "Express", color: toolColor },
+    { name: "React", color: toolColor },
+    { name: "DDD", color: techniqueColor },
+  ],
 };
 
 const Home = () => (
-  <ResponsiveHeading>
+  <div>
+    <Head />
     <Segment id="homeBody" vertical>
       <Container text>
         <p>
-          Aspiring software developer studying CS at UWaterloo. Trying to become
-          a generalist, but secretly has passions for AI and software
-          performance.
+          A 3rd year CS student at UWaterloo spending the prime years of his
+          life watching lectures and working at software engineering co-op
+          internships. In his free time, he can be found leading backend
+          development at <a href="https://loolabs.org/">Loo Labs</a> and being
+          terrible at video games. Favourite tech stack: Google Sheets.
         </p>
       </Container>
 
@@ -180,38 +187,8 @@ const Home = () => (
       </Divider>
       <Container text>
         <Card.Group centered>
-          <Card raised>
-            <Image src={asteria} />
-            <Card.Content>
-              <Card.Header as="a" href="https://github.com/xujustinj/Asteria">
-                Asteria
-              </Card.Header>
-              <Card.Description>
-                A fast machine learning library implemented from scratch.
-                Currently working towards character recognition.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Label color={techniqueColor}>AI</Label>
-              <Label color={languageColor}>C++</Label>
-              <Label color={languageColor}>Python</Label>
-            </Card.Content>
-          </Card>
-          <Card raised>
-            <Image src={stairway_constants} />
-            <Card.Content>
-              <Card.Header>Stairway Constants</Card.Header>
-              <Card.Description>
-                A massive guide to every constant featured on the decorative
-                number line recently installed on UW campus, published across 5
-                installments of UW mathNEWS volume 142.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Label color={techniqueColor}>Writing</Label>
-              <Label color={techniqueColor}>Mathematics</Label>
-            </Card.Content>
-          </Card>
+          <ProjectCard project={Waterpark}></ProjectCard>
+          <ProjectCard project={Asteria}></ProjectCard>
         </Card.Group>
       </Container>
 
@@ -221,101 +198,17 @@ const Home = () => (
       </Divider>
       <Container text>
         <Card.Group centered>
-          <Card raised>
-            <Image src={swift_record_generator} />
-            <Card.Content>
-              <Card.Header
-                as="a"
-                href="https://github.com/xujustinj/swift-record-generator"
-              >
-                Swift Record Generator
-              </Card.Header>
-              <Card.Description>
-                Measures the relative performance of Swift structs, classes, and
-                string-keyed dictionaries, with respect to mutation, copying,
-                (de)serialization, and more.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Label color={languageColor}>Swift</Label>
-            </Card.Content>
-          </Card>
-          <Card raised>
-            <Image src={ulam_spiral} />
-            <Card.Content>
-              <Card.Header
-                as="a"
-                href="https://xujustinj.github.io/Extended-Essay/"
-              >
-                Ulam Spiral
-              </Card.Header>
-              <Card.Description>
-                When you highlight prime numbers on a spiral number line,
-                strange patterns appear. In high school, I wrote a{" "}
-                <a href="https://xujustinj.github.io/Extended-Essay/03-01-19%20Version%20(Revision).pdf">
-                  paper
-                </a>{" "}
-                explaining what causes them, and how they might be useful.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Label color={techniqueColor}>Number Theory</Label>
-              <Label color={languageColor}>Java</Label>
-            </Card.Content>
-          </Card>
-          <Card raised>
-            <Image src={hexagonal_2048} />
-            <Card.Content>
-              <Card.Header
-                as="a"
-                href="https://xujustinj.github.io/Hexagonal-2048/"
-              >
-                Hexagonal 2048
-              </Card.Header>
-              <Card.Description>
-                My introductory project to JavaScript, inspired by my love for
-                the original game. Swipe or use QWEASD to control the board.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Label color={languageColor}>JavaScript</Label>
-              <Label color={toolColor}>p5.js</Label>
-            </Card.Content>
-          </Card>
-          <Card raised>
-            <Image src={adetos} />
-            <Card.Content>
-              <Card.Header
-                as="a"
-                href="https://docs.google.com/spreadsheets/d/1JsttjsZt6USt7h1Z66PhwibUmafxuH1Mwkzk2tNlDag/edit"
-              >
-                ADETOS
-              </Card.Header>
-              <Card.Description>
-                A macro-powered spreadsheet that juggles matchmaking,
-                scorekeeping, and table management for multiple concurrent
-                double elimination tournaments.
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Label color={toolColor}>Spreadsheets</Label>
-              <Label color={languageColor}>Google Apps Script</Label>
-            </Card.Content>
-          </Card>
+          <ProjectCard project={StairwayConstants}></ProjectCard>
+          <ProjectCard project={SwiftRecordGenerator}></ProjectCard>
+          <ProjectCard project={UlamSpiral}></ProjectCard>
+          <ProjectCard project={Hexagonal2048}></ProjectCard>
+          <ProjectCard project={ADETOS}></ProjectCard>
         </Card.Group>
       </Container>
     </Segment>
 
-    <Segment id="homeFooter" inverted vertical>
-      <Container text>
-        <small>
-          Made with React and Semantic UI.
-          <br />
-          Last updated Thursday Feburary 25, 2021.
-        </small>
-      </Container>
-    </Segment>
-  </ResponsiveHeading>
+    <Foot />
+  </div>
 );
 
 export default Home;
